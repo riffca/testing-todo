6b99261593ead17b33cdfdf3f61245e077a0a752
riffca <riffca@yandex.ru>
2020-07-27 13:03:48 +0300

    add app

---
+++ dist/precache-manifest.817ac9b6d2fbe9b98ffbcea31173932b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba34fb2f29ddcca22e76",
+    "url": "/css/app.10f03949.css"
+  },
+  {
+    "revision": "f2bee76289e1a88b1a7c",
+    "url": "/css/chunk-268cd8ca.3b89ad0b.css"
+  },
+  {
+    "revision": "d364f1283e009be3698d3d2340f9b4f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba34fb2f29ddcca22e76",
+    "url": "/js/app.ab7f76c3.js"
+  },
+  {
+    "revision": "f2bee76289e1a88b1a7c",
+    "url": "/js/chunk-268cd8ca.324f5d59.js"
+  },
+  {
+    "revision": "c66fe91463ba40894d69",
+    "url": "/js/chunk-2d22d746.fe9db5dd.js"
+  },
+  {
+    "revision": "3c44337c3b8f69ad55b1",
+    "url": "/js/chunk-vendors.b517087e.js"
+  },
+  {
+    "revision": "2812dba9a4192ea2f68066e808384096",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
